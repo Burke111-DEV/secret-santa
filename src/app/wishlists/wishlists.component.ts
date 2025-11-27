eace61a3d3630d262faa8c7d1592df67432ec512
BurkeDev <burke111.dev@gmail.com>
2025-11-27 22:02:31 +0000

    Fix create wishlist dialog height exceeding viewport.

--- src/app/wishlists/wishlists.component.ts
+++ src/app/wishlists/wishlists.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { SessionService } from '../session.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { SnackbarYesnoComponent } from '../snackbar-yesno/snackbar-yesno.component';
-import { FirestoreService } from '../firestore.service';
-import { WishlistGeneratorComponent } from '../wishlist-generator/wishlist-generator.component';
-import { MatDialog } from '@angular/material/dialog';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {SessionService} from '../session.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {SnackbarYesnoComponent} from '../snackbar-yesno/snackbar-yesno.component';
+import {FirestoreService} from '../firestore.service';
+import {WishlistGeneratorComponent} from '../wishlist-generator/wishlist-generator.component';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-wishlists',
@@ -14,18 +14,19 @@ import { MatDialog } from '@angular/material/dialog';
 export class WishlistsComponent implements OnInit {
   @ViewChild("wishlistsHeaderPanel") wishlistsHeaderPanel: any;
   @ViewChild("wishlistsEmptyPanel") wishlistsEmptyPanel: any;
-  
+
   constructor(public session: SessionService,
               private firestore: FirestoreService,
               public dialog: MatDialog,
-              private snackBar: MatSnackBar) { }
+              private snackBar: MatSnackBar) {
+  }
 
   ngOnInit(): void {
   }
 
   public createWishlist() {
-    const dialogRef = this.dialog.open(WishlistGeneratorComponent, { 
-      width: '85vw', 
+    const dialogRef = this.dialog.open(WishlistGeneratorComponent, {
+      width: '85vw',
       minHeight: '50vh',
       maxHeight: '90vh',
       data: {
@@ -33,24 +34,27 @@ export class WishlistsComponent implements OnInit {
       }
     });
 
-    dialogRef.afterClosed().subscribe(result => { });
+    dialogRef.afterClosed().subscribe(result => {
+    });
   }
 
-  public getLines(text:string) {
+  public getLines(text: string) {
     const lineBreaks = text.match(/(\r\n|\r|\n)/g);
-    return 1+(lineBreaks ? lineBreaks.length : 0);
+    return 1 + (lineBreaks ? lineBreaks.length : 0);
   }
 
   public editWishList(wishlist: string) {
-    const dialogRef = this.dialog.open(WishlistGeneratorComponent, { 
-      width: '85vw', 
+    const dialogRef = this.dialog.open(WishlistGeneratorComponent, {
+      width: '85vw',
       minHeight: '50vh',
+      maxHeight: '90vh',
       data: {
         type: "edit",
         source: wishlist,
       }
     });
-    dialogRef.afterClosed().subscribe(result => { });
+    dialogRef.afterClosed().subscribe(result => {
+    });
   }
 
   private async deleteWishlist(wishlistName: string) {
@@ -77,15 +81,19 @@ export class WishlistsComponent implements OnInit {
         duration: 4000
       });
     });
-    if(response === "OK") {
+    if (response === "OK") {
       this.deleteWishlist(wishlistName);
     }
   }
 
   public doNothing(panelName: string) {
-    switch(panelName) {
-      case "header": { return this.wishlistsHeaderPanel.close(); }
-      case "empty": { return this.wishlistsEmptyPanel.close(); }
+    switch (panelName) {
+      case "header": {
+        return this.wishlistsHeaderPanel.close();
+      }
+      case "empty": {
+        return this.wishlistsEmptyPanel.close();
+      }
     }
   }
 
